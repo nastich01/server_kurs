b7a8c6552fb27cba40df0a235edda5eccfb8ae82
nastich01 <100304524+nastich01@users.noreply.github.com>
2022-05-28 19:24:04 +0300

    Create App.vue

---
+++ src/views/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<main class="container">
+			<router-view></router-view>
+		</main>
+		<alert-snack :text="alertText"></alert-snack>
+	</div>
+</template>
+
+<script>
+	import AlertSnack from "../components/AlertSnack";
+	import { mapState } from 'vuex';
+
+	export default {
+		computed: {
+			...mapState({
+				alertText: state => state.alertText
+			})
+		},
+		components: {
+			AlertSnack
+		}
+	}
+</script>
